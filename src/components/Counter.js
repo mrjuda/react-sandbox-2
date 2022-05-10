4426b367ed094cd65453f9422f8d8835b3544f9e
judah <87juda@gmail.com>
2022-05-10 20:22:56 -0300

    Testing part 2

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,61 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable no-unreachable */
 // Counter.js
 
 import React from 'react';
+import { createStore } from 'redux';
 import './styles/Counter.css';
 
-function Counter() {
+const msg1 = 'Redux reduces complexity';
+
+const Counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return msg1;
+  }
+
+  // const { createStore } = Redux;
+  const store = createStore(Counter);
+
+  console.log(store.getState());
+
+  expect(
+    Counter(0, { type: 'INCREMENT' }),
+  ).toEqual(1);
+
+  expect(
+    Counter(1, { type: 'INCREMENT' }),
+  ).toEqual(2);
+
+  expect(
+    Counter(2, { type: 'DECREMENT' }),
+  ).toEqual(1);
+
+  expect(
+    Counter(1, { type: 'DECREMENT' }),
+  ).toEqual(0);
+
+  expect(
+    Counter(0, { type: 'SOMETHING_ELSE' }),
+  ).toEqual(1);
+
+  expect(
+    Counter(undefined, {}),
+  ).toEqual(0);
+
+  console.log('Tests passed!');
+
   return (
     <div>
       <span>
-        Redux is, indeed, awesome!
+        Redux is awesome!
       </span>
     </div>
   );
-}
+};
 
 export default Counter;
