f76cfd1226d7b34d75f341a74fecd1a03e19bbcf
judah <87juda@gmail.com>
2022-05-10 17:47:02 -0300

    Add redux store

---
+++ src/app/store.js
@@ -0,0 +1,7 @@
+// store.js
+
+import { configureStore } from '@reduxjs/toolkit';
+
+export default configureStore({
+  reducer: {},
+});
--- src/components/wQuote.js
+++ src/components/wQuote.js
@@ -1,29 +1,16 @@
+/* eslint-disable no-unused-vars */
 // wQuote.js
 
 import './styles/wQuote.css';
 import React, { useState, useEffect } from 'react';
 
 function WebQuote() {
-  const [email, setEmail] = useState(() => window.localStorage.getItem('hooksEmail') || '');
-  const [password, setPassword] = useState(() => window.localStorage.getItem('hooksPassword') || '');
-  const handleEmailChange = (e) => setEmail(e.target.value);
-  const handlePasswordChange = (e) => setPassword(e.target.value);
-  useEffect(() => {
-    window.localStorage.setItem('hooksEmail', email);
-    window.localStorage.setItem('hooksPassword', password);
-  });
   return (
     <div className="divQuote">
       <span className="quote">
         Math used to be awesome.
         Then, they invented Calculus I and II. - Judá Teixeira, 2010
       </span>
-      <input value={email} onChange={handleEmailChange} />
-      <input value={password} onChange={handlePasswordChange} />
-      <p>
-        Logged in as:
-        <span>{email}</span>
-      </p>
     </div>
   );
 }
