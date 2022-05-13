6be18be9135d510e03cf038fb8fc53679909eb53
judah <87juda@gmail.com>
2022-05-12 21:40:39 -0300

    Working counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -10,56 +10,68 @@ import './styles/Counter.css';
 const msg1 = 'Redux reduces complexity';
 
 // console.log(store.getState());
-console.log(msg1);
+// console.log(msg1);
 
-const Counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
+const Counter = () => {
+  const counter = (state = 0, action) => {
+    switch (action.type) {
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1;
+      default:
+        return state;
+    }
+  };
 
   // const { createStore } = Redux;
-  const store = createStore(Counter);
+  const store = createStore(counter);
+  // console.log(store.getState());
 
-  console.log(store.getState());
+  const render = () => {
+    console.log('hooray');
+    document.body.innerText = store.getState();
+    // return (
+    //   <div>
+    //     {store.getState}
+    //   </div>
+    // );
+  };
 
-  expect(
-    Counter(0, { type: 'INCREMENT' }),
-  ).toEqual(1);
+  store.subscribe(render);
+  render();
 
-  expect(
-    Counter(1, { type: 'INCREMENT' }),
-  ).toEqual(2);
+  document.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREMENT' });
+  });
 
-  expect(
-    Counter(2, { type: 'DECREMENT' }),
-  ).toEqual(1);
+  // expect(
+  //   counter(0, { type: 'INCREMENT' }),
+  // ).toEqual(1);
 
-  expect(
-    Counter(1, { type: 'DECREMENT' }),
-  ).toEqual(0);
+  // expect(
+  //   counter(1, { type: 'INCREMENT' }),
+  // ).toEqual(2);
 
-  expect(
-    Counter(0, { type: 'SOMETHING_ELSE' }),
-  ).toEqual(1);
+  // expect(
+  //   counter(2, { type: 'DECREMENT' }),
+  // ).toEqual(1);
 
-  expect(
-    Counter(undefined, {}),
-  ).toEqual(0);
+  // expect(
+  //   counter(1, { type: 'DECREMENT' }),
+  // ).toEqual(0);
 
-  console.log('Tests passed!');
+  // expect(
+  //   counter(0, { type: 'SOMETHING_ELSE' }),
+  // ).toEqual(1);
 
-  return (
-    <div>
-      <span>
-        Redux is awesome!
-      </span>
-    </div>
-  );
+  // expect(
+  //   counter(undefined, {}),
+  // ).toEqual(0);
+
+  // console.log('Tests passed!');
+
+  // return counter;
 };
 
 export default Counter;
--- src/components/wRedux.js
+++ src/components/wRedux.js
@@ -6,6 +6,7 @@ import Counter from './Counter';
 function WebRedux() {
   return (
     <div>
+      <h2>Contador redux</h2>
       <Counter />
     </div>
   );
